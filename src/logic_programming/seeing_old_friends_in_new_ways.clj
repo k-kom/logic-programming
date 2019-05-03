e89f9d2299d694635bda1b31500c1f5ee3691d77
k-kom <hal02660@gmail.com>
2019-05-03 19:23:21 +0900

    seeing old friend in new ways

---
+++ src/logic_programming/seeing_old_friends_in_new_ways.clj
@@ -0,0 +1,14 @@
+(ns logic-programming.seeing-old-friends-in-new-ways
+  (:require [clojure.core.logic :refer :all]))
+
+;;48
+(run* [q]
+  (membero 'olive '(virgin olive oil)))
+
+;;49
+(run 1 [y]
+  (membero y '(hummus with pita)))
+
+
+
+
